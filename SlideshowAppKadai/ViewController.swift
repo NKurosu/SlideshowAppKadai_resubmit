43d3f12aeac866d03e64b337255a3181b1cb6173
n.kanreky <n_kanreky_ta@yahoo.co.jp>
2016-11-12 00:41:15 +0900

    Initial Commit

---
+++ SlideshowAppKadai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideshowAppKadai
+//
+//  Created by Naomi Cross on 2016/11/12.
+//  Copyright © 2016年 n.kanreky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
